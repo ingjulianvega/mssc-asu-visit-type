dafae4527e275bf02805c04378915ae5e4d00b63
Julian Vega <ingjulianvega@gmail.com>
2021-07-18 17:24:04 -0500

    :sparkles: Error messages

---
+++ src/main/java/ingjulianvega/ximic/msscasuvisittype/exception/MvcExceptionHandler.java
@@ -0,0 +1,67 @@
+package ingjulianvega.ximic.msscasuvisittype.exception;
+
+import ingjulianvega.ximic.msscasuvisittype.configuration.ErrorCodeMessages;
+import ingjulianvega.ximic.msscasuvisittype.configuration.VisitTypeParameters;
+import ingjulianvega.ximic.msscasuvisittype.web.model.ApiError;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class MvcExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public MvcExceptionHandler(VisitTypeParameters visitTypeParameters) {
+        this.visitTypeParameters = visitTypeParameters;
+    }
+
+    VisitTypeParameters visitTypeParameters;
+
+
+    @ExceptionHandler(VisitTypeException.class)
+    public ResponseEntity<ApiError> validationErrorHandler(VisitTypeException ame) {
+        ApiError apiError = ApiError
+                .builder()
+                .timestamp(LocalDateTime.now())
+                .api(visitTypeParameters.getApi())
+                .apiCode(ame.getApiCode())
+                .error(ame.getError())
+                .message(ame.getMessage())
+                .solution(ame.getSolution())
+                .build();
+         return new ResponseEntity<>(apiError, ame.getHttpStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        ApiError apiError = ApiError
+                .builder()
+                .timestamp(LocalDateTime.now())
+                .api(visitTypeParameters.getApi())
+                .apiCode(ErrorCodeMessages.ARGUMENT_NOT_VALID_API_CODE)
+                .error(ErrorCodeMessages.ARGUMENT_NOT_VALID_ERROR)
+                .message(errors.toString())
+                .solution(ErrorCodeMessages.ARGUMENT_NOT_VALID_SOLUTION)
+                .build();
+
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+}
